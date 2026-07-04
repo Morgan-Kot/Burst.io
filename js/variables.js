b335bea49010a87c04f12d8da1ef348b3ce85fae
Morgan Kot <morgankot36@gmail.com>
2026-07-04 22:28:47 +0200

    Modify bomb odds and base popcoins payout
    Updated bomb odds from 13 to 17 and increased base popcoins payout from 1 to 2.

--- js/variables.js
+++ js/variables.js
@@ -6,18 +6,15 @@
     SPAWN_DELAY_MAX: 1,
     DOUBLE_SPAWN_ODDS: 20,
     BUBBLE_LIFETIME_MS: 2000, 
-
     MAX_DIFFICULTY_LEVEL: 25,
     SPEED_SCALING_FACTOR: 0.07,
-
     POPCOIN_LEVEL_INTERVAL: 1,
     POPCOIN_BASE_PAYOUT: 2,
-
     VIBRATION_PATTERN: [45, 60, 20],
     BOMB_VIBRATION: [200, 100, 200, 100, 200],
     TEST_MODE_MULTIPLIER: 30.0,
     JUNIOR_MULTIPLIER: 2.5,
-    BOMB_ODDS: 13,
+    BOMB_ODDS: 17,
     BOMB_PENALTY: 10
 };
 
@@ -53,5 +50,5 @@ const container = document.getElementById('game-container');
 //debugging and version control
 //every change made in this file has to be documented in the changelog
 console.log("-- Variables.js changelog --")
-console.log("Changed: nothing")
-console.log("Developer notes: nothing")
+console.log("Changed: bomb_odds changed from 13 to 17.")
+console.log("Developer notes: base popcoins payout has been increased from 1 to 2.")
